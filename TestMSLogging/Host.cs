66a40e22bb7a5687b2c490f650887f880829d1b5
Gang Wang <v-gaw@microsoft.com>
2022-05-17 06:35:18 +0000

    Init repo

---
+++ TestMSLogging/Host.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace TestMSLogging
+{
+    internal class Host : IDisposable
+    {
+        private readonly ILoggerFactory logFactoryWithCustomConsole;
+        private readonly ILoggerFactory logFactory;
+
+        internal Host()
+        {
+            logFactoryWithCustomConsole =
+                LoggerFactory.Create(builder =>
+                    builder
+                        .SetMinimumLevel(LogLevel.Trace)
+                        .AddConsole(config => config.FormatterName = nameof(CliConsoleFormatter))
+                        .AddConsoleFormatter<CliConsoleFormatter, ConsoleFormatterOptions>(config =>
+                        {
+                            config.IncludeScopes = true;
+                            config.TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff";
+                        }));
+
+            logFactory =
+                LoggerFactory.Create(builder =>
+                    builder
+                        .SetMinimumLevel(LogLevel.Trace)
+                        .AddSimpleConsole(options =>
+                        {
+                            options.IncludeScopes = true;
+                            options.SingleLine = true;
+                            options.TimestampFormat = "hh:mm:ss ";
+                        }));
+        }
+
+        public ILoggerFactory LogFactoryWithCustomConsole => logFactoryWithCustomConsole;
+        public ILoggerFactory LogFactory => logFactory;
+
+        public void Dispose()
+        {
+            logFactoryWithCustomConsole.Dispose();
+            logFactory.Dispose();
+        }
+    }
+}
---
+++ TestMSLogging/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TestMSLogging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var messages = GenerateMessages();
+            Host host = new Host();
+            Console.WriteLine("Test logging with custom console formatter.");
+            var loggerWithCustomCF = host.LogFactoryWithCustomConsole.CreateLogger<Program>();
+            Log(loggerWithCustomCF, "custom console formatter", messages);
+
+            //Console.WriteLine("Test logging with predefined SimpleConsole formatter.");
+            //var logger = host.LogFactory.CreateLogger("SimpleConsole");
+            //Log(logger, "SimpleConsole formatter", messages);
+        }
+
+        private static void Log(ILogger logger, string formatterName, string[] messages)
+        {
+            using (logger.BeginScope($"[Log with {formatterName}]"))
+            {
+                for (int i = 0; i < messages.Length; i++)
+                {
+                    logger.LogDebug(messages[i]);
+                }
+            }
+        }
+
+        private static string[] GenerateMessages()
+        {
+            int length = 50;
+            var messages = new string[length];
+            for(int i = 0; i < length; i++)
+            {
+                messages[i] = $"This is the message at index: {i} of the list";
+            }
+            return messages;
+        }
+    }
+}
